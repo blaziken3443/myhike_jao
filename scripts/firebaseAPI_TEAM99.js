1557eab129245de1dcba5239cb850c6ce5c614ba
João Eduardo Santos Pollhuber <jpollhuber@outlook.com>
2024-02-20 10:26:32 -0800

    Add Lab 7 content

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDhIiJ5SAOyD7dZfmxNnNjsa4ANfo-KZz8",
+    authDomain: "comp1800-202410-demo-ad8cd.firebaseapp.com",
+    projectId: "comp1800-202410-demo-ad8cd",
+    storageBucket: "comp1800-202410-demo-ad8cd.appspot.com",
+    messagingSenderId: "877701494127",
+    appId: "1:877701494127:web:9d6fd1a619bc4bbcf3a49a"
 };
 
 //--------------------------------------------
